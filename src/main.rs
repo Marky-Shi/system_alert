6dba3c0fbee499e8bf5e3395d37b9fe698d78329
Chengcheng-S <87012234+Chengcheng-S@users.noreply.github.com>
2024-08-05 09:14:53 +0800

    add cpu power

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use system_alert::system_info::{check_exit, get_system_info};
+use system_alert::{
+    cli::{check_exit, check_root},
+    system_info::get_system_info,
+};
 
 fn main() {
+    check_root();
+
     let receiver = check_exit();
     if let Err(err) = get_system_info(receiver) {
         eprintln!("Error: {}", err);
